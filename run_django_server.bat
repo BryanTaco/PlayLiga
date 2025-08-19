4b351bba0679c4e584f09b52043f159de74333ec
BryanTaco <bstaco@puce.edu.ec>
2025-08-19 10:27:57 -0500

    Fix Python environment for WebContainer

--- run_django_server.bat
+++ run_django_server.bat
@@ -1,13 +1,11 @@
 @echo off
 REM Script para iniciar el servidor Django en modo desarrollo con reinicio automático
 
-REM Activar entorno virtual si existe (ajustar ruta si es necesario)
-if exist "venv\Scripts\activate.bat" (
-    call venv\Scripts\activate.bat
-)
+REM Usar Python directamente sin entorno virtual en WebContainer
+echo Iniciando servidor Django...
 
 REM Ejecutar servidor Django con autoreload
-python manage.py runserver
+python3 manage.py runserver
 
 REM Mantener ventana abierta
 pause
